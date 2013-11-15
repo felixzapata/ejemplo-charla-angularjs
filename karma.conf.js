8ed049e8726cb70fe5bd40ca1696e462b8e32a0e
Felix Zapata <fzberlinches@gmail.com>
2013-11-16 00:14:23 +0100

    changed karma conf based on grunt-karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,68 @@
 // Karma configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+    config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
-];
+        // base path, that will be used to resolve files and exclude
+        basePath: '.',
 
-// list of files to exclude
-exclude = [];
+        // list of files / patterns to load in the browser
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-scenario/angular-scenario.js',
+            'app/scripts/*.js',
+            'app/scripts/**/*.js',
+            'test/spec/directives/main.js'
+        ],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+        // list of files to exclude
+        exclude: [],
 
-// web server port
-port = 8080;
+        frameworks: ['jasmine'],
 
-// cli runner port
-runnerPort = 9100;
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'growl'
+        reporters: ['progress'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // web server port
+        port: 9876,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+        // cli runner port
+        runnerPort: 9100,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_ERROR,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Chrome'],
+
+
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
+};
