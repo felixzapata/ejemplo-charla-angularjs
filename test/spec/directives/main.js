a8b7a3e684e581e41963371c74a88bb06e3fae0f
Felix Zapata <fzberlinches@gmail.com>
2013-11-16 22:32:48 +0100

    basic test for directive

--- test/spec/directives/main.js
+++ test/spec/directives/main.js
@@ -2,10 +2,7 @@
 (function() {
     'use strict';
 
-    describe('Directive: coordinates', function() {
-
-        // load the controller's module
-        beforeEach(module('example1App'));
+    describe('Directive: myCoordinates', function() {
 
         var fakeData = [{
             'id': 'DGT7',
@@ -28,22 +25,32 @@
             'latitude': 40.4671642,
             'longitude': -3.8704629
         }],
-            scope, compile, element;
+            $scope, element;
+
+        // load the controller's module
+        beforeEach(module('views/coordinates.html'));
+        beforeEach(module('dir.my.coordinates'));
+        
+        
 
         // Initialize the controller and a mock scope
-        beforeEach(inject(function($injector) {
-            scope = $injector('$rootScope');
-            compile = $injector('$compile');
-            element = compile('<my-coordinates http-coordinates="myCoordinates"></my-coordinates>')(scope);
-            scope.myCoordinates = fakeData;
-            scope.$digest();
-        }));
+        beforeEach(function() {
+            inject(function($compile, $rootScope) {
+                $scope = $rootScope;
+                element = $compile('<my-coordinates http-coordinates="myFakeCoordinates"></my-coordinates>')($scope);
+                $scope.myFakeCoordinates = fakeData;
+                $scope.$digest();
+            });
+        });
 
         it('should have a table element', function() {
-            expect(element.find('table').length).toBe(1);
+            expect(element[0].nodeName).toBe('TABLE');
         });
-        it('should have 5 table rows', function() {
-            expect(element.find('table').length).toBe(5);
+        it('should have 6 table rows', function() {
+            expect(element.find('tr').length).toBe(6);
+        });
+        it('should have 5 table columns', function() {
+            expect(element.find('td').length).toBe(15);
         });
     });
 }());
