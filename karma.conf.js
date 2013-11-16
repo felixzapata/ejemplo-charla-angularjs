44f2dfae0532d5d75089d3c3bb55f25266a8b461
Felix Zapata <fzberlinches@gmail.com>
2013-11-16 21:03:54 +0100

    refactor based on angular generator after installed karma generator

--- karma.conf.js
+++ karma.conf.js
@@ -1,46 +1,38 @@
 // Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
 
 module.exports = function(config) {
     config.set({
-
         // base path, that will be used to resolve files and exclude
-        basePath: '.',
+        basePath: '',
+
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
 
         // list of files / patterns to load in the browser
         files: [
             'app/bower_components/angular/angular.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-resource/angular-resource.js',
+            'app/bower_components/angular-cookies/angular-cookies.js',
+            'app/bower_components/angular-sanitize/angular-sanitize.js',
             'app/bower_components/angular-route/angular-route.js',
-            'app/bower_components/angular-scenario/angular-scenario.js',
             'app/scripts/*.js',
             'app/scripts/**/*.js',
-            'test/spec/directives/main.js'
+            'test/spec/**/*.js',
+            'app/views/*.html',
         ],
 
-        // list of files to exclude
+        // list of files / patterns to exclude
         exclude: [],
 
-        frameworks: ['jasmine'],
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress', 'growl'
-        reporters: ['progress'],
-
         // web server port
-        port: 9876,
-
-
-        // cli runner port
-        runnerPort: 9100,
-
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
+        port: 8080,
 
         // level of logging
         // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-        logLevel: config.LOG_ERROR,
+        logLevel: config.LOG_INFO,
+
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: false,
@@ -55,10 +47,6 @@ module.exports = function(config) {
         // - PhantomJS
         // - IE (only Windows)
         browsers: ['Chrome'],
-
-
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
 
 
         // Continuous Integration mode
